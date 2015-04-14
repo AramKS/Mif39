41a36d7305315fd007ed1413f43814188a5217d0
AramKS <aram.kamo.saakyan@gmail.com>
2015-04-14 14:46:07 +0200

    new begin

---
+++ C++/Assets/assets.hpp
@@ -0,0 +1,14 @@
+#ifndef ASSETS_HPP
+#define ASSETS_HPP
+
+#include "Utility/utility.hpp"
+#include "AssetInterfaces/interfaces.hpp"
+#include "AssetGenerics/generics.hpp"
+
+#include "Assets/Image.hpp"
+#include "Assets/Material.hpp"
+#include "Assets/Mesh.hpp"
+#include "Assets/behaviour.hpp"
+
+#endif // ASSETS_HPP
+
