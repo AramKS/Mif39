00cf7b330e06aaa297d4d73dc2d2ce1e99b41a37
Djuu <cybermica@hotmail.com>
2015-04-14 17:16:35 +0200

    ajout connexion multi thread

--- C++/Graal/GraalAssetServer/main.cpp
+++ C++/Graal/GraalAssetServer/main.cpp
@@ -3,29 +3,37 @@
 
 #include "TcpNetworking/simpletcpstartpoint.hpp"
 
+int nb_Client_connect;
 
-void prin(){
-    std::cout<<"Test thread !! "<<std::endl;
+void connection_Client (QUuid client, SimpleTcpStartPoint *server){
+    while ( true ) {
+        ByteBuffer message;
+        server->receive(client,message); std::cout<<client.toString().toStdString()<<std::endl;//std::cout << "Recv : " << message.getLength() << " bytes" << std::endl;
+        server->send(client,message); std::cout << "Sent : " << message.getLength() << " bytes" << std::endl;
+    }
 }
 
 int main ( int argc, char** argv ) {
-    std::cout<<"lol"<<std::endl;
+    nb_Client_connect = 0;
+    std::thread th_client[10];
+    std::thread th_listen;
+
     QUuid fake;
     SimpleTcpStartPoint::Options options;
     options.connectionPort = 3000;
-    options.maximumConnectedClients = 1;
+    options.maximumConnectedClients = 2;
     SimpleTcpStartPoint server ( options );
     server.start();
     QUuid client;
-    std::thread first(prin);
-    std::cout<<first.get_id()<<std::endl;
-    while ( client == fake ) {
-        client = server.listen();
-    }
+
     while ( true ) {
-        ByteBuffer message;
-        server.receive(client,message);// std::cout << "Recv : " << message.getLength() << " bytes" << std::endl;
-        server.send(client,message); //std::cout << "Sent : " << message.getLength() << " bytes" << std::endl;
+        client = server.listen();
+        bool insert = true;
+        if (client != fake){
+            th_client[nb_Client_connect] = std::thread (connection_Client, client, &server/**/);
+            nb_Client_connect++;
+            std::cout<<"Client "<<nb_Client_connect<<" est connecté"<<endl;
+        }
     }
     server.stop ();
     return 0;
