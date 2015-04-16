963737fb829f15bcd7d09ce5fac2b92aa88cc8c6
Djuu <cybermica@hotmail.com>
2015-04-16 09:34:52 +0200

    transfert d'un cube par le server , lecture du cube par client , OK

--- C++/Graal/GraalAssetClient/main.cpp
+++ C++/Graal/GraalAssetClient/main.cpp
@@ -57,8 +57,38 @@ int main ( int argc, char** argv ) {
 
     SharedResourcePtr ptr = ResourceHolder::FromBuffer(message,d);
 
-   /* std::cout<<ptr2->getName().toStdString()<<std::endl;*/
+    std::cout<<ptr->getName().toStdString()<<std::endl;
+    ptr->Usage();
+    std::cout<<"Vertices : "<<std::endl;
+    Vector3d * p = ptr->get < Vector3d* > ( "Vertices");
+    for(int i=0;i<ptr->get < unsigned int > ( "#Vertices");i++){
+        std::cout<<p[i].x<<" ";
+        std::cout<<p[i].y<<" ";
+        std::cout<<p[i].z<<std::endl;
+    }
 
+    std::cout<<"Triangles : "<<std::endl;
+    Triangle * t = ptr->get < Triangle* > ( "Faces");
+    for(int i=0;i<ptr->get < unsigned int > ( "#Faces");i++){
+        std::cout<<t[i].m_vertexIndices[0]<<" // "<<t[i].m_normalIndices[0]<<"  ";
+        std::cout<<t[i].m_vertexIndices[1]<<" // "<<t[i].m_normalIndices[1]<<"  ";
+        std::cout<<t[i].m_vertexIndices[2]<<" // "<<t[i].m_normalIndices[2]<<std::endl;
+    }
+
+    std::cout<<"Normales : "<<std::endl;
+    Vector3d * n = ptr->get < Vector3d* > ( "Normals");
+    for(int i=0;i<ptr->get < unsigned int > ( "#Normals");i++){
+        std::cout<<n[i].x<<" ";
+        std::cout<<n[i].y<<" ";
+        std::cout<<n[i].z<<std::endl;
+    }
+
+    std::cout<<"textures : "<<std::endl;
+    Vector2d * tex = ptr->get < Vector2d* > ( "TexVertices");
+    for(int i=0;i<ptr->get < unsigned int > ( "#TexVertices");i++){
+        std::cout<<tex[i].x<<" ";
+        std::cout<<tex[i].y<<std::endl;
+    }
 
     client.close ();
 
--- C++/Utility/includes.hpp
+++ C++/Utility/includes.hpp
@@ -8,6 +8,9 @@
 #include <vector>
 #include <string>
 
+#include <thread>
+#include <pthread.h>
+
 #include <QLocale>
 
 #include <QObject>
